546ae44c7d33edb32759ef4dc3e42ff9f11193c6
Kevin Ernst <ernstki@mail.uc.edu>
2018-07-09 21:05:54 -0400

    Minor stylistic fixes

--- notchfinder.py
+++ notchfinder.py
@@ -7,20 +7,23 @@ import requests
 import lxml.html
 
 NCBI_BASE_URL= 'https://www.ncbi.nlm.nih.gov'
-NCBI_SEARCH_URL = 'pubmed/?term='
+NCBI_SEARCH_PATH = 'pubmed/?term='
 NCBI_SEARCH_TERM = 'notch'
 
 
 if __name__ == '__main__':
     with requests.Session() as s:
-        response = s.get(NCBI_BASE_URL + '/' + NCBI_SEARCH_URL
-                         + NCBI_SEARCH_TERM)
+        response = s.get("{base}/{path}{term}".format(
+            base=NCBI_BASE_URL,
+            path=NCBI_SEARCH_PATH,
+            term=NCBI_SEARCH_TERM
+        ))
 
         assert response.ok
 
         # see: https://stackoverflow.com/a/15622069
         html = lxml.html.fromstring(response.content)
-        html.xpath('//p[@class="title"]/a')
+        anchors = html.xpath('//p[@class="title"]/a')
 
-        for anchor in html.xpath('//p[@class="title"]/a'):
+        for anchor in anchors:
             print(NCBI_BASE_URL + anchor.attrib['href'])
